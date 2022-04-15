945bc32b37f18c2c3ff72001ad974992788bf391
Chandra178_ <chandra@mplus.software>
2022-04-15 20:11:05 +0700

    'add'

---
+++ client/pages/cards/[cardId].tsx
@@ -0,0 +1,31 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import { InferGetStaticPropsType } from 'next'
+
+export default function Project({ card }: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(card);
+  return <div>{card}</div>;
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const data = params?.cardId;
+  return {
+    props: {
+      card: data,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // path name type must string
+  const characters = [{ name: "jack" }, { name: "king" }];
+  return {
+    paths: characters.map((character) => {
+      return {
+        params: {
+          cardId: character.name,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
